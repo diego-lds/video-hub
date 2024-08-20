044847bdb3692c676d95bfcaa8fa007d798d5abe
Diego Lopes <diegolopes087@gmail.com>
2024-08-20 11:13:57 -0300

    LImpando tela inicial

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,12 @@
-import NextLogo from "./NextLogo";
-import SupabaseLogo from "./SupabaseLogo";
-
 export default function Header() {
   return (
     <div className="flex flex-col gap-16 items-center">
       <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
         <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
       </div>
       <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
       <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
+        Texto inicial do aplicativo criado usando{" "}
         <a
           href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
           target="_blank"
@@ -28,7 +15,7 @@ export default function Header() {
         >
           Supabase
         </a>{" "}
-        and{" "}
+        e{" "}
         <a
           href="https://nextjs.org/"
           target="_blank"
