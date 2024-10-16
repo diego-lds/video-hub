0ddfdbf5f04c33df91acb2000a1516dab11fa13f
Diego Lopes <diegolopes087@gmail.com>
2024-10-16 13:09:07 -0300

    mains content layout fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,16 +34,7 @@ export default function RootLayout({
           <Navbar />
           <main className="max-w-5xl mx-auto ">{children}</main>
 
-          <footer className="w-full h-30  border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs bg-emerald-600">
-            <p>
-              <a
-                className="text-white hover:underline"
-                href="https://github.com/diego-lds"
-              >
-                Criado por Diego Lopes &copy; {new Date().getFullYear()}
-              </a>
-            </p>
-          </footer>
+          <Footer />
         </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -9,36 +9,35 @@ export default async function Index() {
   return (
     <>
       <main className="flex flex-col lg:flex-row">
-        <div>
-          <Image width={474} height={125} src="/logo.png" alt="Logo" />
-
-          <h3>Crie e disponibilize seus cursos online com facilidade!</h3>
-          <p>
-            Transforme seu conhecimento em uma fonte de renda, compartilhando
-            suas habilidades com o mundo. Nossa plataforma oferece todas as
-            ferramentas necessárias para que você possa desenvolver, hospedar e
-            vender seus cursos online, sem complicações técnicas.
-          </p>
+        <div className="flex items-center">
+          <div>
+            <Image width={474} height={125} src="/logo.png" alt="Logo" />
+            <h3>Crie e disponibilize seus cursos online com facilidade!</h3>
+            <p>
+              Transforme seu conhecimento em uma fonte de renda, compartilhando
+              suas habilidades com o mundo. Nossa plataforma oferece todas as
+              ferramentas necessárias para que você possa desenvolver, hospedar
+              e vender seus cursos online, sem complicações técnicas.
+            </p>
+          </div>
+          <Image src="/hero.svg" alt="Hero" width={500} height={500} />
         </div>
-        <Image src="/hero.svg" alt="Hero" width={500} height={500} />
       </main>
 
       {courses && (
         <section>
           <h1>Todos os cursos</h1>
           <Separator />
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
-            <ul>
-              {courses.map((course) => (
-                <Card
-                  key={course.id}
-                  imagePath={course.image_path}
-                  title={course.title}
-                  description={course.description}
-                />
-              ))}
-            </ul>
-          </div>
+          <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+            {courses.map((course) => (
+              <Card
+                key={course.id}
+                imagePath={course.image_path}
+                title={course.title}
+                description={course.description}
+              />
+            ))}
+          </ul>
         </section>
       )}
     </>
